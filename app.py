07d3512b73d452b9f83a537d59566b5871118984
Carlos Arturo <carlosarturo.castrocastro@gmail.com>
2026-03-22 11:30:49 -0500

    Agregar ruta y template CRUD Cliente

--- app.py
+++ app.py
@@ -19,7 +19,9 @@ app.register_blueprint(producto_bp)
 app.register_blueprint(usuario_bp)
 
 from routes.persona import bp as persona_bp
+from routes.cliente import bp as cliente_bp
 app.register_blueprint(persona_bp)
+app.register_blueprint(cliente_bp)
 
 
 if __name__ == '__main__':
